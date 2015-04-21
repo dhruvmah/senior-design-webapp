e927506353a3c7381e11f601f297d74d7e4c2544
Dhruv Maheshwari <dhwari@gmail.com>
2015-04-20 22:01:56 -0400

    webapp

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var mongoose = require("mongoose");
+
+var uristring =
+	process.env.MONGOLAB_URI ||
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/senior-design';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+//controllers
+var controller = require('./controllers/controller.js');
+
+//other boiler plate
+app.use(express.static(__dirname + '/public'));
+app.use(express.logger("default"));
+app.use( express.cookieParser() );
+app.use(express.session({secret:'session'}));
+app.use(express.bodyParser())
+   .use(express.methodOverride())
+   .use(app.router)
+   .use(express.multipart());
+
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+//General Online
+app.get("/", controller.index);
+app.get("/cluster", controller.cluster);
+
+module.exports = app;
+
---
+++ controllers/controller.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var async = require("async");
+/*--------------  General Functions ----------- */
+
+
+/**
+ * Opens admin/volunteer homepage based on login credentials
+ * Redirects to login page if invalid credentials
+ */
+
+exports.index = function(req, res) {
+    republican_domains =  ["Saab", "Volvo", "BMW", "Saab", "Volvo", "BMW", "Saab", "Volvo", "BMW", "BMW"];
+    democrat_domains =  ["Saab", "Volvo", "BMW", "Saab", "Volvo", "BMW", "Saab", "Volvo", "BMW", "BMW"];;
+    res.render("home.ejs", {republican_domains: republican_domains, democrat_domains: democrat_domains});
+};
+
+exports.cluster = function(req, res) {
+    clusterOne = ["a", "b", "c", "d"];
+    res.send(clusterOne);   
+}
